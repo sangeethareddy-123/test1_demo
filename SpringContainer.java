a6698eff63456f46bceb5f51692ddba5a4f60872
sangeethareddy-123 <61237013+sangeethareddy-123@users.noreply.github.com>
2021-01-20 08:20:00 +0530

    Added Spring Files

---
+++ SpringContainer.java
@@ -0,0 +1,21 @@
+package com.springexample.springex.springhello;
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class SpringContainer {
+
+        private static final String template = "Hello, %s!";
+        private final AtomicLong counter = new AtomicLong();
+
+        @GetMapping("/greeting")
+        public Framework greeting(@RequestParam(value = "name", defaultValue = "Sangeetha") String name) {
+            return new Framework(counter.incrementAndGet(), String.format(template, name));
+        }
+    }
+
---
+++ SpringexApplication.java
@@ -0,0 +1,15 @@
+package com.springexample.springex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class SpringexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringexApplication.class, args);
+	}
+
+}
