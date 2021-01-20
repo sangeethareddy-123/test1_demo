74972bb3649038e581fdfa6186f3ce8c9c24bc39
sangeethareddy-123 <61237013+sangeethareddy-123@users.noreply.github.com>
2021-01-20 09:43:48 +0530

    added spring project

---
+++ springex/src/main/java/com/springexample/springex/springhello/Framework.java
@@ -0,0 +1,27 @@
+package com.springexample.springex.springhello;
+
+public class Framework {
+    private long id;
+    private String content;
+
+    public Framework(long id,String content){
+        this.id=id;
+        this.content=content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
